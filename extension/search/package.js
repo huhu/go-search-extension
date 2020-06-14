4d9f99a0c9251b5a6d8887dc4c4101258ae5b995
Folyd <lyshuhow@gmail.com>
2020-06-14 17:01:17 +0800

    Fix package search repository and name undefined issue

--- extension/search/package.js
+++ extension/search/package.js
@@ -1,3 +1,5 @@
+const LOWEST_MATCH_INDEX = 999;
+
 function PackageSearch(packagesIndex) {
     this.packages = packagesIndex;
     this.packageNames = Object.keys(packagesIndex);
@@ -10,18 +12,19 @@ PackageSearch.prototype.search = function(keyword) {
         let [domain, repository, name] = rawName.split("/");
         let domainMatchIndex = domain.replace(/[-_]/ig, "").indexOf(keyword);
         if (domainMatchIndex === -1) {
-            domainMatchIndex = 999;
+            domainMatchIndex = LOWEST_MATCH_INDEX;
         }
-        let repositoryMatchIndex = repository.replace(/[-_]/ig, "").indexOf(keyword);
+        let repositoryMatchIndex = repository ? repository.replace(/[-_]/ig, "").indexOf(keyword) : LOWEST_MATCH_INDEX;
         if (repositoryMatchIndex === -1) {
-            repositoryMatchIndex = 999;
-        }
-        let nameMatchIndex = name.replace(/[-_]/ig, "").indexOf(keyword);
-        if (nameMatchIndex === -1) {
-            nameMatchIndex = 999;
+            repositoryMatchIndex = LOWEST_MATCH_INDEX;
         }
 
-        if ([domainMatchIndex, repositoryMatchIndex, nameMatchIndex].some(i => i !== 999)) {
+        let nameMatchIndex = name ? name.replace(/[-_]/ig, "").indexOf(keyword) : LOWEST_MATCH_INDEX;
+        if (nameMatchIndex === -1) {
+            nameMatchIndex = LOWEST_MATCH_INDEX;
+        }
+
+        if ([domainMatchIndex, repositoryMatchIndex, nameMatchIndex].some(i => i !== LOWEST_MATCH_INDEX)) {
             let [description, version] = this.packages[rawName];
             result.push({
                 domainMatchIndex,
@@ -38,7 +41,7 @@ PackageSearch.prototype.search = function(keyword) {
     return result.sort((a, b) => {
         if (a.domainMatchIndex === b.domainMatchIndex) {
             if (a.repositoryMatchIndex === b.repositoryMatchIndex) {
-                if (a.nameMatchIndex === b.nameMatchIndex) {
+                if (a.nameMatchIndex === b.nameMatchIndex && a.name && b.name) {
                     return a.name.length - b.name.length;
                 }
                 return a.nameMatchIndex - b.nameMatchIndex;
