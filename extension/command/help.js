2de4225f1a6965eb84fe4a658bd99407b47d9305
Folyd <lyshuhow@gmail.com>
2020-06-20 22:34:29 +0800

    Update awesome index and help message

--- extension/command/help.js
+++ extension/command/help.js
@@ -7,6 +7,7 @@ class HelpCommand extends Command {
         const value = [
             `Prefix ${c.match(":")} to execute command (:help, :history, etc)`,
             `Prefix ${c.match("!")} to search packages exclusively, prefix ${c.match("!!")} to open the repository`,
+            `Prefix ${c.match("$")} to search a curated list of awesome Go frameworks, libraries and software`,
         ];
         return this.wrap(value);
     }
